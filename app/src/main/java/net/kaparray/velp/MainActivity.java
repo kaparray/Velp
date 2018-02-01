71894394f19f612b28099d2314a1c664d4043a44
kaparray <kaparray@gmail.com>
2018-02-01 23:10:25 +0300

    I change all U error's

--- app/src/main/java/net/kaparray/velp/MainActivity.java
+++ app/src/main/java/net/kaparray/velp/MainActivity.java
@@ -83,13 +83,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_task) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_bonus) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_chat) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_info) {
 
         } else if (id == R.id.nav_share) {
 
