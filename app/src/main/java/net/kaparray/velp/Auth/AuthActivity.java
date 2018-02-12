41a5dbaa1ee53763c987f8c603288b53c16c8887
kaparray <kaparray@gmail.com>
2018-02-12 22:30:17 +0300

    Add animation, fix auth bug

--- app/src/main/java/net/kaparray/velp/Auth/AuthActivity.java
+++ app/src/main/java/net/kaparray/velp/Auth/AuthActivity.java
@@ -4,12 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,10 +37,12 @@ public class AuthActivity extends AppCompatActivity{
     TextView mForgetPas;
     public static final String TAG = "Email_Login_Incorrect";
 
+    private Animation anim;
+    private ImageView imageView;
+
     @Override
     public void onStart() {
         super.onStart();
-
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null ){
@@ -62,31 +68,43 @@ public class AuthActivity extends AppCompatActivity{
 
         mForgetPas = findViewById(R.id.tv_forgetPass);
 
+        imageView = (ImageView) findViewById(R.id.iv_ic_app);
+        anim = AnimationUtils.loadAnimation(this, R.anim.rotate_animaton);
 
         mLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mAuth.signInWithEmailAndPassword(mEmail.getText().toString(), mPassword.getText().toString())
-                        .addOnCompleteListener(AuthActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "signInWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                                    imm.hideSoftInputFromWindow(mLogIn.getWindowToken(),
-                                            InputMethodManager.HIDE_NOT_ALWAYS);
-                                    // Go to MainActivity!!
-                                    MainPage();
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(AuthActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
+                // Animation
+                imageView.startAnimation(anim);
+                // Hide keyboard
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(mLogIn.getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+
+                try{
+                    mAuth.signInWithEmailAndPassword(mEmail.getText().toString(), mPassword.getText().toString())
+                            .addOnCompleteListener(AuthActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        // Go to MainActivity!!
+                                        MainPage();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(AuthActivity.this, "Authentication failed. Please try again!",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
                                 }
-                            }
-                        });
+                            });
+                } catch (Exception e) {
+                    Log.e("Error", "Error auth");
+                    showMessage(R.string.ToastInSingIn
+                    );
+                }
             }
         });
 
@@ -111,12 +129,14 @@ public class AuthActivity extends AppCompatActivity{
     }
 
 
-
-
-
     public void MainPage(){
-        Intent intent = new Intent(AuthActivity.this ,MainActivity.class);
+        Intent intent = new Intent(AuthActivity.this, MainActivity.class);
         startActivity(intent);
+        finish();
+    }
+
+    private void showMessage(@StringRes int string) {
+        Toast.makeText(AuthActivity.this, string, Toast.LENGTH_LONG).show();
     }
 
 }
