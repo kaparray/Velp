94832dd74e55e16673d0ce5aa6062f27e0b152ad
kaparray <kaparray@gmail.com>
2018-05-25 22:54:58 +0300

    Add new bug (joke), Add ButterKnife, fix bug with photo

--- app/src/main/java/net/kaparray/velp/fragments/ProfileFragment.java
+++ app/src/main/java/net/kaparray/velp/fragments/ProfileFragment.java
@@ -1,14 +1,7 @@
 package net.kaparray.velp.fragments;
 
 
-import android.accounts.Account;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Parcelable;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -17,15 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInApi;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -33,19 +19,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
 
 import net.kaparray.velp.MainActivity;
 import net.kaparray.velp.R;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
-import static android.content.Context.MODE_PRIVATE;
 
 public class ProfileFragment extends Fragment{
 
@@ -95,22 +71,31 @@ public class ProfileFragment extends Fragment{
 
 
 
-                if(photo.equals("ic_boy")){
-                    mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_boy));
-                } else if(photo.equals("ic_boy1")){
-                    mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_boy1));
-                }else if(photo.equals("ic_girl")){
-                    mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_girl));
-                }else if(photo.equals("ic_girl1")){
-                    mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_girl1));
-                }else if(photo.equals("ic_man1")){
-                    mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_man1));
-                }else if(photo.equals("ic_man2")){
-                    mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_man2));
-                }else if(photo.equals("ic_man3")){
-                    mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_man3));
-                }else if(photo.equals("ic_man4")){
-                    mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_man4));
+                try {
+                    if (photo.equals("ic_boy")) {
+                        mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_boy));
+                    } else if (photo.equals("ic_boy1")) {
+                        mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_boy1));
+                    } else if (photo.equals("ic_girl")) {
+                        mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_girl));
+                    } else if (photo.equals("ic_girl1")) {
+                        mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_girl1));
+                    } else if (photo.equals("ic_man1")) {
+                        mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_man1));
+                    } else if (photo.equals("ic_man2")) {
+                        mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_man2));
+                    } else if (photo.equals("ic_man3")) {
+                        mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_man3));
+                    } else if (photo.equals("ic_man4")) {
+                        mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_man4));
+                    }
+                }catch (Exception w){
+                    try {
+                        mPhotoUser.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher_round));
+                    }catch (Exception e){
+                        Log.d("Error-Message", e.getStackTrace() + "");
+                    }
+
                 }
 
 
